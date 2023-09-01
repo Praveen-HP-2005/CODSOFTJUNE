06ce0c7739427bb82f27ba82ea5a69ab5a330890
Praveen-HP-2005 <135138571+Praveen-HP-2005@users.noreply.github.com>
2023-09-01 20:03:40 +0530

    Todo List.py
    This is the first task given to me as a intern in Codsoft

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
+import customtkinter as ctk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+task_widgets = {}
 
+def add_todo():
+    todo = entry.get()
+    if todo:
+        label = ctk.CTkLabel(scrollable_frame, text=todo)
+        label.pack()
+        delete_button = ctk.CTkButton(scrollable_frame, text="Delete", command=lambda label=label: delete_todo(label))
+        delete_button.pack()
+        task_widgets[label] = delete_button
+        entry.delete(0, ctk.END)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def delete_todo(label_to_delete):
+    delete_button = task_widgets.get(label_to_delete)
+    if delete_button:
+        delete_button.destroy()
+        label_to_delete.destroy()
+        del task_widgets[label_to_delete]
 
+def add_todo_on_enter(event):
+    if event.keysym == 'Return':
+        add_todo()
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+root = ctk.CTk()
+root.geometry("750x450")
+root.title("Todo App")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+title_label = ctk.CTkLabel(root, text="Daily Tasks", font=ctk.CTkFont(size=30, weight="bold"))
+title_label.pack(padx=10, pady=(40, 20))
+
+scrollable_frame = ctk.CTkScrollableFrame(root, width=500, height=200)
+scrollable_frame.pack()
+
+entry = ctk.CTkEntry(scrollable_frame, placeholder_text="Add todo")
+entry.pack(fill="x")
+entry.bind("<Return>", add_todo_on_enter)
+
+add_button = ctk.CTkButton(root, text="Add", width=500, command=add_todo)
+add_button.pack(pady=20)
+
+root.mainloop()
